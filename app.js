29bb5426dd18a02817d366426297f043d2b25377
Filip Koza <peacefilip1989@gmail.com>
2021-07-30 19:17:24 +0200

    m;jhlkjfh

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ const authRouter = require('./router/api/authRouter')
 const filesRouter = require('./router/api/filesRouter')
 
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', authRouter)
